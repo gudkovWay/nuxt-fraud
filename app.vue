bbf39564b1125124530883b1928adbbf6d2418c4
gudkovWay <gudkoviurii@gmail.com>
2024-10-24 14:39:56 +0300

    feature: prettier & default global styles

--- app.vue
+++ app.vue
@@ -1,3 +1,7 @@
+<script>
+import './styles/global.scss';
+</script>
+
 <template>
   <NuxtPage />
 </template>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
-import pluginVue from 'eslint-plugin-vue'
+import withNuxt from './.nuxt/eslint.config.mjs';
+import pluginVue from 'eslint-plugin-vue';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default withNuxt(
   ...pluginVue.configs['flat/recommended'],
+  eslintPluginPrettierRecommended,
 
   {
     files: ['**/*.ts', '**/*.tsx'],
     rules: {
       'no-console': 'error',
-      'indent': ['error', 2]
-    }
+      indent: ['error', 2],
+    },
   },
-)
+);
